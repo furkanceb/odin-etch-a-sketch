058c8c39e6cba9c6a485fcf78c3d8fe1fcff6701
furkanceb <117816558+furkanceb@users.noreply.github.com>
2023-05-05 21:03:45 +0300

    Style 16X16 board

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+
 for(let j=0;j<16;j++){
     const divGrid = document.createElement("div");
     const container=document.querySelector(".container");
     container.appendChild(divGrid);
-
     for(let i=0;i<16;i++){
         const divGridInside = document.createElement("div");
+        divGridInside.style.border="2px solid red";
+        divGridInside.style.width="40px"
+        divGridInside.style.height="40px"
         divGrid.appendChild(divGridInside);
     }
     }
